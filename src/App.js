bf296554018041626a0049b551ee7bf5552a5f26
sher singh rana <mishranirajk121@gmail.com>
2024-07-24 11:23:43 +0530

    [ADDED]react-app

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ContactForm from './ContactForm';
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Contact Us</h1>
+        <ContactForm />
+      </header>
+    </div>
+  );
+};
+
+export default App;
